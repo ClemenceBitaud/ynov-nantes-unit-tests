29d5e016d4ff67e042c855d8fd6abec199148a55
Simon Billet <simon.billet@etudiant.univ-lr.fr>
2022-04-21 14:50:56 +0200

    Mise en place des tests unitaires (5 par fichiers)

--- 1-intro/workshop/unit-tests/tests/TempTrackerTest.php
+++ 1-intro/workshop/unit-tests/tests/TempTrackerTest.php
@@ -6,6 +6,40 @@ require_once('./src/TempTracker.php');
 
 class TempTrackerTest extends TestCase {
     public function test_something() {
-        $this->assertEqual(42, 42);
+        $this->assertEquals(42, 42);
+    }
+
+    public function test_insert() {
+        $tempTracker = new TempTracker();
+        $tempTracker->insert(12);
+
+        $this->assertEquals($tempTracker->get_temps(), [12]);
+    }
+
+    public function test_get_min_no_value(){
+        $tempTracker = new TempTracker();
+        $tempTracker->insert(12);
+        $tempTracker->insert(8);
+        $tempTracker->insert(23);
+
+        $this->assertEquals($tempTracker->get_min(), 8);
+    }
+
+    public function test_get_max_no_value(){
+        $tempTracker = new TempTracker();
+        $tempTracker->insert(12);
+        $tempTracker->insert(8);
+        $tempTracker->insert(23);
+
+        $this->assertEquals($tempTracker->get_max(), 23);
+    }
+
+    public function test_get_mean(){
+        $tempTracker = new TempTracker();
+        $tempTracker->insert(12);
+        $tempTracker->insert(10);
+        $tempTracker->insert(20);
+
+        $this->assertEquals($tempTracker->get_mean(), 14);
     }
 }
--- 1-intro/workshop/unit-tests/tests/flatten_test.php
+++ 1-intro/workshop/unit-tests/tests/flatten_test.php
@@ -8,6 +8,26 @@ class FlattenTest extends TestCase
 {
     public function test_something()
     {
-        $this->assertEqual(1337, 1337);
+        $this->assertEquals(1337, 1337);
+    }
+
+    public function test_one_data()
+    {
+        $this->assertEquals(flatten(["velo"]), ["velo"]);
+    }
+
+    public function test_two_data()
+    {
+        $this->assertEquals(flatten([1337, 23]), [1337, 23]);
+    }
+
+    public function test_nested_once()
+        {
+            $this->assertEquals(flatten(["r", 5, [true, 2, 3]]), ["r", 5, true, 2, 3]);
+        }
+
+    public function test_nested_multiple()
+    {
+        $this->assertEquals(flatten([4, [5, [6, 7]], [1, 2, 3]]), [4, 5, 6, 7, 1, 2, 3]);
     }
 }
