c1c68b4de349d8a7560c8e4b0de7a5ee0177f56c
ClemenceBitaud <clemence.bitaud@gmail.com>
2022-04-21 14:55:55 +0200

    [TEST] Test if I can send a message OK

--- 1-intro/workshop/e2e-tests/tests/some_test.js
+++ 1-intro/workshop/e2e-tests/tests/some_test.js
@@ -2,4 +2,8 @@ Feature('Ynov Nantes');
 
 Scenario('Test Ynov Nantes Land Page', ({ I }) => {
     I.amOnPage('https://www.ynov-nantes.com/');
+    I.click('.cta');
+    I.fillField('.searchfield', 'info');
+    I.wait(5);
+    I.see('Bachelor Informatique');
 });
