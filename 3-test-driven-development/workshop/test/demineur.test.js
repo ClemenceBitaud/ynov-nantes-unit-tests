48ae36f790f34b69622fe2c8fefcca887df7d0d2
Lily <lily.majani@ynov.com>
2022-04-28 10:41:55 +0200

    [Test] entrée première ligne

---
+++ 3-test-driven-development/workshop/test/demineur.test.js
@@ -0,0 +1,9 @@
+describe("Demineur test", function() {
+    it("première ligne entrée", function() {
+        const demineur = new Demineur(2, 3)
+        expect(demineur.n).toBeLessThanOrEqual(100);
+        expect(demineur.n).toBeGreaterThan(0);
+        expect(demineur.m).toBeLessThanOrEqual(100);
+        expect(demineur.m).toBeGreaterThan(0);
+    });
+});
