0b256a47796606921fc3b712ec3badd9171fd9ae
Lily <lily.majani@ynov.com>
2022-04-28 14:25:36 +0200

    [Test + FEATURE] test taille grille (en direct)

--- 3-test-driven-development/workshop/src/demineur.js
+++ 3-test-driven-development/workshop/src/demineur.js
@@ -2,8 +2,20 @@ class Demineur {
     constructor(n, m){
         this.n = n;
         this.m = m;
+        this.grille = [];
+        this.generateGrille();
+    }
+
+    generateGrille() {
+        for(let i = 0; i < this.n; i++) {
+            this.grille[i] = [];
+            for(let j = 0; j < this.m; j++) {
+                this.grille[i][j] = '.';
+            }
+        }
     }
 }
+
 
 module.exports = {
     Demineur
--- 3-test-driven-development/workshop/test/demineur.test.js
+++ 3-test-driven-development/workshop/test/demineur.test.js
@@ -1,10 +1,26 @@
 const { Demineur } = require("../src/demineur")
 describe("Demineur test", function() {
-    it("première ligne entrée", function() {
-        const demineur = new Demineur(2, 3)
+    it("première ligne entrée ok", function() {
+        const demineur = new Demineur(2, 3);
         expect(demineur.n).toBeLessThanOrEqual(100);
         expect(demineur.n).toBeGreaterThan(0);
         expect(demineur.m).toBeLessThanOrEqual(100);
         expect(demineur.m).toBeGreaterThan(0);
+    });
+
+    // test taille grille
+    it("check grid dimension", function() {
+        // Given
+        const n = 4;
+        const m = 4;
+
+        // When
+        const demineur = new Demineur(n, m);
+
+        // Then
+        expect(demineur.grille.length).toBe(n);
+        demineur.grille.forEach(ligne => {
+            expect(ligne.length).toBe(m);
+        });
     });
 });
